cbf0aa072b78ea1a2ffd4bda34ae6b4f9bc728d4
Ngan <lehongngan.ag@gmail.com>
2026-04-23 15:36:49 +0700

    update project

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.static(path.join(process.cwd(), "src", "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
